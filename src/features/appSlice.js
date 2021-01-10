adc1ddd81c6a5195240e6aa0cff13c9d88ebf4e0
Sophia Jung <sophiabraddock82@gmail.com>
2021-01-10 17:32:29 -0500

    firebase setup

---
+++ src/features/appSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+  
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { incrementByAmount } = appSlice.actions;
+
+export const incrementAsync = amount => dispatch => {
+  setTimeout(() => {
+    dispatch(incrementByAmount(amount));
+  }, 1000);
+};
+
+
+export const selectCount = state => state.app.value;
+
+export default appSlice.reducer;
