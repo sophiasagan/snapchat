c03d36558301dc6272ebb38e4b796118d979f60f
Sophia Jung <sophiabraddock82@gmail.com>
2021-01-10 19:44:58 -0500

    preview page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
-import WebcamCapture from './WebcamCapture';
+import React from "react";
+import "./App.css";
+import WebcamCapture from "./WebcamCapture";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Preview from "./Preview";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Snapchat</h1>
-      <WebcamCapture />
+    <div className="app">
+      <Router>
+        <div className="app__body">
+          <Switch>
+          <Route path="/preview">
+              <Preview />
+            </Route>
+            <Route exact path="/">
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -3,6 +3,8 @@ import Webcam from "react-webcam";
 import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 import { useDispatch } from "react-redux";
 import { setCameraImage } from "./features/cameraSlice";
+import { useHistory } from "react-router-dom";
+import "./Webcam.css"
 
 const videoConstraints = {
   width: 250,
@@ -13,11 +15,13 @@ const videoConstraints = {
 function WebcamCapture() {
   const webcamRef = useRef(null);
   const dispatch = useDispatch();
+  const history = useHistory(); 
   //   const [image, setImage] = useState(null);
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     dispatch(setCameraImage(imageSrc));
+    history.push('/preview')
     // console.log(imageSrc);
     // setImage(imageSrc)
   }, [webcamRef]);
